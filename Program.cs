9879ab7eb6645b47333370ff6e0de1332a90c31b
Sai Naga Srujana Vanarasi <W0856682@myscc.ca>
2024-06-14 13:22:41 -0400

    seed data file and program.cs file is updated..its working

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Pen.Data;
+using Pen.Models;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<PenContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("PenContext") ?? throw new InvalidOperationException("Connection string 'PenContext' not found.")));
@@ -9,6 +10,12 @@ builder.Services.AddDbContext<PenContext>(options =>
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
